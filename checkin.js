944775afa17d3a55ae0410ea0dec7cfc2f562268
Yahya <yahya.codin@gmail.com>
2022-08-02 18:30:57 +0300

    Initial commit

--- checkin.js
+++ checkin.js
@@ -1,25 +1,24 @@
 const modal = document.querySelector('#check-number')
 const checkIn = document.querySelector('#check-in')
 const searchNumber = document.querySelector('#number-input')
-const close = document.querySelector('#close')
+const closeModal = document.querySelector('#close')
 const cancelModal = document.querySelector('.btn-close')
 const patientDropdown = document.querySelector('#patients-dropdown')
 const dropdownList = document.querySelector('#patient-list')
 const serviceDropdown = document.querySelector('#services-dropdown')
 const serviceList = document.querySelector('#service-list')
 
-
 searchNumber.onclick = function () {
     modal.style.display = 'block'
   }
 
-close.onclick = function () {
-    modal.style.display = 'none'
-  }
+// closeModal.onclick = function () {
+//     modal.style.display = 'none'
+//   }
 
-  cancelModal.onclick = function () {
-    modal.style.display = 'none'
-    }
+  // cancelModal.onclick = function () {
+  //   modal.style.display = 'none'
+  //   }
 
     window.onclick = function (event) {
         if(event.target === modal){
